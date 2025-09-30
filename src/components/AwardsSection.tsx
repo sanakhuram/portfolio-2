3483d680f5757fb6fdc4123fd1935c6614ef4b4b
Sana Khuram <sana.khuram.baig@gmail.com>
2025-09-30 15:02:10 +0200

    edited Awardssection

--- src/components/AwardsSection.tsx
+++ src/components/AwardsSection.tsx
@@ -8,21 +8,22 @@ export default function AwardsSection() {
 
   return (
     <section className="w-full min-h-screen flex flex-col items-center justify-center px-6 md:px-20 py-20 bg-yellow-500 text-white">
-      <h2 className={`${headingFont.variable} text-4xl font-bold mb-12`}>Awards</h2>
-
-      <h3 className="text-2xl font-semibold mb-2">Golden Egg Award</h3>
-      <p className="mb-6 text-lg font-medium">Best Student Work
- Front-end Development - Online 2025</p>
-
-      {/* Toggle Button */}
+      <div className="flex flex-col items-center">
+        <div className="mb-8 text-center">
+          <h2 className={`${headingFont.variable} text-4xl sm:text-5xl font-extrabold mb-2`}>
+            Awards & Recognition
+          </h2>
+          <p className={`${headingFont.variable} text-lg sm:text-xl max-w-2xl`}>
+            Honored to receive the Golden Egg Award for Best Student Work in Front-end Development (2025). A testament to my dedication and passion for creating exceptional digital experiences.
+          </p>
+        </div>
+        </div>
       <button
         onClick={() => setShowPdf(prev => !prev)}
-        className="mt-4 px-8 py-2  text-white border-2 font-bold rounded-lg hove:shadow-lg transition"
+        className="mt-4 px-8 py-2  text-white border-2 border-dotted font-bold rounded-lg hove:shadow-lg transition"
       >
         {showPdf ? "Hide " : "View "}
       </button>
-
-      {/* PDF iframe (conditionally rendered) */}
       {showPdf && (
         <div className="w-full max-w-4xl h-[600px] bg-white rounded-lg shadow-lg overflow-hidden mt-6">
           <iframe
@@ -33,5 +34,6 @@ export default function AwardsSection() {
         </div>
       )}
     </section>
+  
   );
 }
